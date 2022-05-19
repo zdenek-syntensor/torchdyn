3ca3d2c8c2f173dcd16c97a08a4529e15c0059bb
Zdenek Hynek <zdenek@syntensor.com>
2022-05-19 16:43:03 +0100

    Rename sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -106,9 +106,9 @@ html_theme = 'pt_lightning_sphinx_theme'
 html_theme_options = {
 	'logo': '_static/torchdyn_logo.png',
 }
-html_theme_path = ['../../../lightning_sphinx_theme']
+html_theme_path = ['../../../torchdyn_sphinx_theme']
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
